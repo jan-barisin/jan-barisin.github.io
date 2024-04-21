b15f27d6afbbafb2d820663a30f3abd4ad055c53
jan-barisin <167613379+jan-barisin@users.noreply.github.com>
2024-04-21 19:28:38 +0200

    Calculator

---
+++ script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("display");
+
+let number1; 
+let number2;
+
+let result;
+
+let numArray = []
+let numArray2 = []
+
+
+
+
+
+function addNumber(num) {
+    display.value += input;
+}
+
+function addOperator(input) {
+    display.value = input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    
+}
